eeaa5ed01edfae851f58cbcdd5f67643ef784d39
szymondlugolecki <szymon.dlugolecki77@gmail.com>
2023-11-06 18:10:24 +0100

    added list style circle

--- src/routes/oferta/okregowa-stacja-kontroli-pojazdow/+page.svelte
+++ src/routes/oferta/okregowa-stacja-kontroli-pojazdow/+page.svelte
@@ -1,24 +1,25 @@
 <h1 class="text-4xl font-extrabold tracking-tight scroll-m-20 lg:text-5xl">
-	Okręgowa Stacja Kontroli Pojazdów
+	Okręgowa Stacja Kontroli Pojazdów WCI 008
 </h1>
 
 <div>
 	<span class="text-xl font-semibold tracking-tight scroll-m-20"
-		>W Okręgowej Stacji Kontroli Pojazdów INTERHOME można wykonać następujące badania techniczne:</span
+		>W Okręgowej Stacji Kontroli Pojazdów WCI 008 INTERHOME można wykonać następujące badania
+		techniczne:</span
 	>
 	<ul class="ml-6 list-disc [&>li]:mt-1">
 		<li>
 			Okresowe badania techniczne wszystkich pojazdów samochodowych: samochody osobowe, samochody
-			ciężarowe, autobusy, ciągniki siodłowe i balastowe, samochody specjalne i ciężarowe specjalne.
+			ciężarowe, autobusy, ciągniki siodłowe i balastowe, samochody specjalne i ciężarowe specjalne,
 		</li>
-		<li>Okresowe badania techniczne przyczep, naczep, ciągników rolniczych i motocykli.</li>
+		<li>Okresowe badania techniczne przyczep, naczep, ciągników rolniczych i motocykli,</li>
 		<li>
 			Pierwsze badania techniczne przeprowadzane przed pierwszą rejestracją wszystkich rodzajów i
-			typów pojazdów rejestrowanych po raz pierwszy w Polsce.
+			typów pojazdów rejestrowanych po raz pierwszy w Polsce,
 		</li>
 		<li>
 			Dodatkowe badania techniczne wszystkich rodzajów i typów pojazdów:
-			<ul class="ml-6 [&>li]:mt-1">
+			<ul class="pl-6 [&>li]:pt-1 list-[circle]">
 				<li>pojazdów skierowanych do badań przez organ kontroli ruchu drogowego,</li>
 				<li>
 					pojazdów skierowanych do badań przez wydział komunikacji po zmianie stanu faktycznego
