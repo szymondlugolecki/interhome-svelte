46be765f958964f1a8ab704657eb918eb97944a6
szymondlugolecki <szymon.dlugolecki77@gmail.com>
2023-11-11 14:33:44 +0100

    spelling error nr 2

--- src/routes/kontakt/+page.svelte
+++ src/routes/kontakt/+page.svelte
@@ -19,7 +19,7 @@
 			version: 'weekly'
 		});
 
-		const mapElement = document.getElementById('mapa');
+		const mapElement = document.getElementById('map');
 
 		if (mapElement) {
 			loader
@@ -78,7 +78,7 @@
 			id="mapa"
 		>
 			<div class="flex justify-center w-full overflow-x-auto rounded-lg">
-				<div id="mapa" class="w-[1216px] h-[450px]" />
+				<div id="map" class="w-[1216px] h-[450px]" />
 				<!-- <iframe
 					title="Mapa dojazdu do Interhome"
 					src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2408.542285868978!2d20.6028713!3d52.8666473!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471c2ec13d0ccdc1%3A0xd22ced45baf77712!2sMazowiecka%206%2C%2006-400%20Ciechan%C3%B3w!5e0!3m2!1spl!2spl!4v1689539529109!5m2!1spl!2spl"
