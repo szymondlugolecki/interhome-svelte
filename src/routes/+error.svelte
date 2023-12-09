a070292e2e073239250609804abbbe06bde9fc71
szymondlugolecki <szymon.dlugolecki77@gmail.com>
2023-12-09 22:10:52 +0100

    fixed error page layout

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -4,7 +4,7 @@
 	const httpStatusList = {
 		400: 'Nieprawidłowe żądanie',
 		401: 'Nieautoryzowany dostęp',
-		404: 'Nie znaleziono strony',
+		404: 'Nie znaleziono podstrony. Zapraszamy na stronę główną.',
 		500: 'Wewnętrzny błąd serwera'
 	};
 
@@ -13,7 +13,7 @@
 		$page.status in httpStatusList ? httpStatusList[$page.status] : $page.error.message;
 </script>
 
-<div class="flex flex-col items-center justify-center">
+<div class="flex flex-col items-center justify-center pt-24">
 	<h1 class="text-4xl font-extrabold tracking-tight scroll-m-20 lg:text-5xl">
 		Błąd {$page.status}
 	</h1>
