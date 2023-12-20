e777a96c7491d37fcad698309a4991a6b3e1996a
szymondlugolecki <szymon.dlugolecki77@gmail.com>
2023-12-20 20:54:16 +0100

    Update layout alignment in oferta route

--- src/routes/oferta/+layout.svelte
+++ src/routes/oferta/+layout.svelte
@@ -1,4 +1,4 @@
-<div class="flex justify-center min-h-screen pt-24 pb-12">
+<div class="flex justify-start min-h-screen pt-24 pb-12">
 	<div class="flex flex-col items-start justify-start px-4 gap-y-6 max-w-7xl sm:px-6 lg:px-8">
 		<slot />
 	</div>
