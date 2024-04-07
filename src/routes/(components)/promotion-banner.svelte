4493085d69b7f446d77d9c108fd7f75fd2ae952c
szymondlugolecki <szymon.dlugolecki77@gmail.com>
2024-04-07 15:19:57 +0200

    Update promotion banner image

--- src/routes/(components)/promotion-banner.svelte
+++ src/routes/(components)/promotion-banner.svelte
@@ -1,70 +1,57 @@
 <script lang="ts">
 	import Image from '$components/custom/Util/image.svelte';
-	import Banner from '$assets/banner.jpg?w=750&format=avif;webp;jpg&as=picture';
 	import Button from '$components/ui/button/button.svelte';
 	import { fade } from 'svelte/transition';
+	import Banner from '$assets/baner.jpg?w=1000&format=avif;webp;jpg&as=picture';
+	import { onNavigate } from '$app/navigation';
 
 	let visible = false;
-	setTimeout(() => {
+	const timeout = setTimeout(() => {
 		visible = true;
 	}, 1000);
+
+	onNavigate(() => {
+		console.log('navigating');
+		clearTimeout(timeout);
+	});
 </script>
 
-<!-- animate-in duration-1000 delay-1000 -->
-
 {#if visible}
-	<section
-		class="sticky bottom-5 left-0 right-0 w-full px-2.5 sm:px-5 pb-1.5 z-40 overflow-hidden"
+	<div
 		in:fade={{
-			duration: 1000,
-			delay: 1000
+			duration: 750,
+			delay: 0
 		}}
 		out:fade={{
 			duration: 300,
 			delay: 0
 		}}
+		class="relative shadow-xl banner"
 	>
-		<div
-			class="w-full h-full rounded-md flex sm:flex-row flex-col gap-x-6 gap-y-3 px-2.5 sm:px-5 py-3 shadow-lg overflow-hidden bg-background"
-		>
-			<!-- <div class="hidden sm:flex absolute -top-0.5 right-0.5 sm:top-2 sm:right-2">
-				<p class="text-red-500 font-bold text-lg">Limitowana oferta!</p>
-			</div> -->
-			<div class="flex-1 flex flex-col justify-start gap-y-2 sm:gap-y-4 h-full">
-				<div class="">
-					<h1
-						class="scroll-m-20 border-b pb-1 sm:pb-2 text-2xl font-semibold tracking-tight transition-colors first:mt-0"
-					>
-						Przygotujemy Twoje auto na wiosnę
-					</h1>
-				</div>
-				<div>
-					<ul class="pl-6 list-disc gap-y-2 columns-3 sm:text-base text-sm">
-						<li>Wymiana opon</li>
-						<li>Konserwacja opon</li>
-						<li>Komputerowe ustawienie geometrii kół 3D</li>
-						<li>Przechowalnia opon</li>
-						<li>Przegląd klimatyzacji</li>
-						<li>Wymiana czynnika i oleju w klimatyzacji</li>
-						<li>Odgrzybianie klimatyzacji</li>
-						<li>Sprawdzanie szczelności układu klimatyzacji</li>
-					</ul>
-				</div>
-				<!-- <div class="absolute left-0 top-0 bottom-0 opacity-75 -ml-5 z-10">
-				<Image
-					meta={Banner}
-					alt="tło baneru promocyjnego"
-					sizes="500px"
-					width="500px"
-					height="147px"
-				/>
-			</div> -->
-			</div>
-			<div class="flex flex-col justify-center sm:w-auto flex-grow max-w-[128px]">
-				<Button class="w-[120px]" variant="default" on:click={() => (visible = false)}
-					>Zamknij</Button
-				>
-			</div>
+		<div class="w-72 sm:w-[550px]">
+			<Image
+				class="object-cover object-center w-full h-full"
+				loading="eager"
+				meta={Banner}
+				alt="baner reklamowy"
+			/>
 		</div>
-	</section>
+		<!-- <button
+			on:click={() => (visible = false)}
+			class="absolute top-0 right-0 p-1 font-bold text-white sm:p-2 sm:text-lg">Zamknij</button
+		> -->
+		<button
+			on:click={() => (visible = false)}
+			class="absolute bottom-0 right-0 px-2 py-2 text-lg font-bold text-white">Zamknij</button
+		>
+	</div>
 {/if}
+
+<style>
+	.banner {
+		position: fixed;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
+</style>
